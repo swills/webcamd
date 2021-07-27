14e9e9df6394128edfe00c3c0ec561710c25c4d1
Steve Wills <steve@mouf.net>
2021-07-27 13:01:37 -0400

    webcamd: Also write pidfile after calling daemon(3)
    So that pid number is for child instead of parent. pidfile is written
    twice, so that it's not wrong if we're not forking, but it's no problem
    because child pid is written last and parent always exits if forking.
    
    Signed-off-by: Steve Wills <steve@mouf.net>

--- webcamd.c
+++ webcamd.c
@@ -698,6 +698,7 @@ pidfile_create(int bus, int addr, int index)
 	if (local_pid == NULL) {
 		return (EEXIST);
 	} else {
+		/* in case called from kernel/linux_usb.c:usb_linux_probe_p() */
 		pidfile_write(local_pid);
 	}
 
@@ -855,6 +856,7 @@ main(int argc, char **argv)
 		}
 		if (daemon(0, 0) != 0)
 			v4b_errx(EX_USAGE, "Cannot daemonize");
+		pidfile_write(local_pid);
 	}
 	atexit(&v4b_exit);
 
